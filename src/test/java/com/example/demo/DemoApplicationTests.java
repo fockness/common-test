2d31a1c3baa99a095c49714d02b85c178d4234e7
fockness <1162496378@qq.com>
2020-07-02 16:18:04 +0800

    新增redis配置

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import com.example.demo.cases.designpattern.strategy.bestpractices.StrategyServiceFactory;
+import com.example.demo.cases.designpattern.strategy.bestpractices.StrategyType;
+import com.example.demo.cases.util.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +16,21 @@ public class DemoApplicationTests {
 	@Autowired
 	private StrategyServiceFactory strategyServiceFactory;
 
+	@Autowired
+	private RedisUtil redisUtil;
+
 	@Test
 	public void contextLoads() {
 	}
 
 	@Test
 	public void testStrategy() {
-		//strategyServiceFactory.getMessageService(StrategyType.SMS).sendMessage("Hello world");
+		strategyServiceFactory.getMessageService(StrategyType.SMS).sendMessage("Hello world");
+	}
 
-
+	@Test
+	public void testRedis(){
+		redisUtil.set("aa", "aaa");
+		System.out.println(redisUtil.get("aa"));
 	}
 }
